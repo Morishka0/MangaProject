c4203d02dcf61dd18d268f45c905ebcbeff47bc3
barghest0 <bolshov03@mail.ru>
2021-08-13 23:23:15 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.sass'
+
+function App() {
+    return (
+        <div className='page'>
+            <header className='header'>
+
+            </header>
+        </div>
+    )
+}
+
+export default App
